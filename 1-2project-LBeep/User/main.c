206e2d5a72e2d51a10e0ea170683e6143449012e
starrysky <3181793776@qq.com>
2026-04-22 15:25:17 +0800

    Add files via upload

---
+++ 1-2project-LBeep/User/main.c
@@ -0,0 +1,16 @@
+#include "stm32f10x.h"                  // Device header
+
+int main(void)
+{
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
+	GPIO_InitTypeDef GPIO_InitStruct;
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_12;
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOB, &GPIO_InitStruct);
+	
+	while (1)
+	{
+		GPIO_SetBits(GPIOB, GPIO_Pin_12);
+	}
+}
